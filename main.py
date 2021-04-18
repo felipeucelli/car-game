8ae7be2f69b4df19296a77b0e46abeacfb6df5bb
felipeucelli <78266261+felipeucelli@users.noreply.github.com>
2021-04-18 17:15:08 -0400

    Added new car

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 screen_width = 520
 screen_height = 600
@@ -33,6 +34,22 @@ class Car(pygame.sprite.Sprite):
         self.pos_move = 0
 
 
+class Pipe(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('images/car2.png').convert_alpha()
+
+        self.rect = self.image.get_rect()
+        self.rect[1] = -100
+        self.rect[0] = randint(100, 400)
+        self.rect[3] = 600
+
+    def update(self):
+        self.rect[1] += 10
+
+
 class Road(pygame.sprite.Sprite):
 
     def __init__(self, y_pos):
@@ -59,6 +76,10 @@ car_group = pygame.sprite.Group()
 car = Car()
 car_group.add(car)
 
+pipe_group = pygame.sprite.Group()
+pipe = Pipe()
+pipe_group.add(pipe)
+
 road_group = pygame.sprite.Group()
 for i in range(2):
     road = Road(screen_height * i)
@@ -82,7 +103,7 @@ while game:
             if event.key == pygame.K_RIGHT:
                 car.move_right()
 
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_DOWN:
                 car.freeze()
 
     screen.blit(screen, (0, 0))
@@ -93,11 +114,19 @@ while game:
         new_road = Road(screen_height)
         road_group.add(new_road)
 
+    if off_screen(pipe_group.sprites()[0]):
+        pipe_group.remove(pipe_group.sprites()[0])
+
+        new_pipe = Pipe()
+        pipe_group.add(new_pipe)
+
     road_group.update()
     car_group.update()
+    pipe_group.update()
 
     road_group.draw(screen)
     car_group.draw(screen)
+    pipe_group.draw(screen)
 
     pygame.display.update()
 
